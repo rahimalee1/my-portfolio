a651d28a29aeda926f517429b40769eaedb08ab8
rahimalee1 <rahimgilal1@gmail.com>
2025-08-28 19:23:14 +0500

    New update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -121,7 +121,7 @@ const Navbar = () => {
               <li className="pt-1">
                 {/* Resume button (mobile) */}
                 <a
-                  href="/resume.pdf"
+                  href="/Muhammad Rahim.pdf"
                   download
                   className="btn-videobox w-full justify-center text-[16px] font-medium"
                   onClick={() => setToggle(false)}
